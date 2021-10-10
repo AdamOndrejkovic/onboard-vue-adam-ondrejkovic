1ca926bc46fb5be66c8a52e2ce3164da533eb8dc
Adam Ondrejkovic <adam.ondrejkovic@gmail.com>
2021-10-10 19:59:28 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,147 @@
 <template>
   <main>
-    <!-- Your code goes here -->
+<!--    While loading show spinner-->
+    <div v-if="loading" class="wrapper">
+      <div class="loader"></div>
+    </div>
+<!--    If error occurs show an message-->
+    <div v-else-if="error" class="wrapper">
+      <div class="error"><p>Error occurred please try again later.</p></div>
+    </div>
+<!--    If data loads and no error is caught displays users-->
+    <div v-else-if="!error || !loading" class="cardWrapper">
+      <div class="filter"> Filter: <input v-on:keydown.esc="clearInput" type="text" placeholder="Filter Users" v-model="searchQuery"></div>
+      <div class="p-3">
+        <hr>
+        Selected: {{selected}} <br>
+        Unselected: {{unselected}}
+        <hr>
+      </div>
+      <div class="pt-3 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-5 lg:grid-cols-3 gap-3">
+        <!--      Loop through data and for each user create a card-->
+        <UserCard v-for="user in filteredUser" :key="user.id" @changeSelectionCounter="changeSelectionCounter($event)"
+                  :name="user.username"
+                  :email="user.email"
+                  :company="user.company.name"
+                  :phone="user.phone"
+        />
+        <!--    No data found message    -->
+        <p class="noData" v-if="filteredUser.length === 0">No users where found.</p>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
 import { inject } from "vue";
-
+import UserCard from "./components/UserCard.vue";
 export default {
-  setup() {
-    const axios = inject("$axios");
-
-    return {};
+  components: {UserCard},
+  data: function () {
+    return {
+      error: false,
+      loading: true,
+      userList: [],
+      searchQuery: '',
+      selected: 0,
+      unselected: 0
+    }
   },
+  computed:{
+    filteredUser: function () {
+      const query = this.searchQuery.toLowerCase();
+      if (this.searchQuery === ""){
+        return this.userList;
+      }
+      return this.userList.filter((user) => {
+        return user.username.toLowerCase().includes(query.toLowerCase())
+      })
+    }
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+    loadData: function (){
+      const axios = inject("$axios");
+      axios.get('https://jsonplaceholder.typicode.com/users')
+          .then((response) => {
+            setTimeout(() => {
+              this.userList = response.data;
+              this.loading = false;
+              this.unselected = response.data.length;
+            }, 2000)
+          })
+          .catch((error) => {
+            setTimeout(() => {
+              this.error = true;
+              this.loading = false;
+            }, 2000)
+          });
+    },
+    clearInput: function () {
+      this.searchQuery = '';
+    },
+    changeSelectionCounter: function (event) {
+      if (event){
+        this.selected ++;
+        this.unselected --;
+      }else{
+        this.selected --;
+        this.unselected ++;
+      }
+    }
+  }
 };
 </script>
+
+<style scoped>
+.wrapper div, .noData {
+  position: absolute;
+  top: 45%;
+  left: 45%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+.error p {
+  margin: auto;
+  padding-top: 55px;
+  font-size: 32px;
+  letter-spacing: 0.5rem;
+  color: red;
+}
+.loader {
+  border: 16px solid #f3f3f3;
+  border-radius: 50%;
+  border-top: 16px solid #3498db;
+  width: 150px;
+  height: 150px;
+  animation: spin 2s linear infinite;
+}
+
+@keyframes spin {
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+}
+
+.filter {
+  margin: auto;
+  text-align: center;
+  padding: 25px 10px;
+}
+
+.filter input {
+  border: 1px solid grey;
+  border-radius: 25px;
+  padding-left: 10px;
+}
+
+.noData{
+  left: 50%;
+  text-align: center;
+  color: red;
+  font-size: 18px;
+}
+
+</style>
---
+++ src/components/UserCard.vue
@@ -0,0 +1,56 @@
+<template>
+<div class="userCard" v-bind:class="{selected: selected}" v-on:click="changeSelection">
+  <div class="image">
+    <img src="src/assets/images/profile.svg" alt="Image">
+  </div>
+  <div class="main text-center">
+    <ul class="list-none">
+      <li>{{name}}</li>
+      <li>{{email}}</li>
+      <li>{{company}}</li>
+      <li>{{phone}}</li>
+    </ul>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props:['name', 'company', 'email', 'phone'],
+  name: "UserCard",
+  data: function() {
+    return {
+      selected: false
+    }
+  },
+  methods: {
+    changeSelection: function () {
+      this.selected =  !this.selected;
+      this.$emit('changeSelectionCounter', this.selected);
+    }
+  }
+}
+</script>
+
+<style scoped>
+.userCard{
+  padding: 15px 10px;
+  border: 2px cornflowerblue solid;
+  border-radius: 25px;
+  width: 300px;
+  margin: auto;
+}
+
+.selected {
+  border: 2px green solid;
+}
+
+.image img {
+  width: 125px;
+  margin: auto;
+}
+
+.main {
+  padding: 15px 5px 5px;
+}
+</style>
